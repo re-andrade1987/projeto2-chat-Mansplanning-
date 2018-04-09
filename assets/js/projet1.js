2a7f4f4cbbe2a17585e70d264b3c3982bb792528
Renata Andrade <renataandrade1987@hotmail.com>
2018-04-09 16:56:57 -0300

    projetojavascript

---
+++ assets/js/projet1.js
@@ -0,0 +1,59 @@
+function btn(){
+    window.location.replace("pergunta-02.html");
+	
+}
+
+function btn1(){
+    window.location.replace("pergunta-03.html");
+	
+}
+
+function btn2(){
+    window.location.replace("pergunta-04.html");
+	
+}
+
+function btn3(){
+    window.location.replace("pergunta-05.html");
+	
+}
+
+function btn4(){
+    window.location.replace("pergunta-06.html");
+	
+}
+
+function btn5(){
+    window.location.replace("pergunta-07.html");
+	
+}
+
+function btn6(){
+    window.location.replace("pergunta-08.html");
+	
+}
+
+function btn7(){
+    window.location.replace("pergunta-09.html");
+	
+}
+
+function btn8(){
+    window.location.replace("pergunta-10.html");
+	
+}
+
+
+function btn9(){
+    window.location.replace("resultado.html");
+	
+}
+
+function iniciar(){
+    window.location.replace("index.html");
+	
+}
+
+function resultado(){
+    var acertos = document.getElementById("acertos").value;
+}
